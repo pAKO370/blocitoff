5cd9e3378e4bbe3ba1658bf94a9540621e58f716
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-31 12:15:34 -0600

    needs wotking items display

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,13 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   def create
-    @item = Item.new
-    @item.name = params[:item][:name]
+    @user = User.find(params[:user_id])
+    @item = @user.items.new(params[:name])
+    #@new_item = Item.new
 
     if @item.save
       flash[:notice] = "Item saved"
-      redirect_to @item
+      redirect_to items_index_path
     else
       flash.now[:alert] = "There was a problem saving your item"
       render :new
