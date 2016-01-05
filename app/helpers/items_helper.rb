469d3c3ba58cbbbb1200817fed7d4a07dccbec01
Phil Fluegel <pfluegelcx@gmail.com>
2016-01-05 09:57:48 -0600

    working time remaining

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,8 @@
 module ItemsHelper
+  def time_remaining(item)
+    time_dif = Time.now - item.created_at
+    from_time = Time.now
+    distance_of_time_in_words(time_dif, 7.days - time_dif)
+  end
+
 end
